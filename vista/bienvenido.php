d992e72f8d947c9794b0d6af6dd74f505f0b833f
Juankmilo01 <48554005+Juankmilo01@users.noreply.github.com>
2019-03-21 17:08:59 -0500

    j

--- vista/bienvenido.php
+++ vista/bienvenido.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet"  type="text/css" href="../vista/css/header.css">
     <link rel="stylesheet"  type="text/css" href="../vista/css/footer.css">
     <link rel="stylesheet" type="text/css" href="../vista/css/login.css">
+    <link rel="stylesheet" type="text/css" href="../vista/css/bienvenido.css">
    
 
     
@@ -31,27 +32,26 @@
 <br>
 <br>
 
+<div class="cuadroentradas">
+<div class="container-entrada">
+            <div class="mision">
+                <img class="logo" src="../vista/img/LOGO SENA.png" alt="">
+            <h1>Misión</h1>
+               <p>El SENA está  encargado de cumplir la función que le corresponde al Estado de invertir en el desarrollo social y 
+                   técnico de los trabajadores colombianos, ofreciendo y ejecutando la formación profesional integral, para la 
+                   incorporación y el desarrollo de las personas en actividades productivas que contribuyan al desarrollo social, 
+                   económico y tecnológico del país.​</p>
+                   <h1> Visión</h1>
+                   <p>En el 2018 el SENA será reconocido por la efectividad de su gestión, sus aportes al empleo decente y a ​la
+                        generación de ingresos, impactando la productividad de las personas y de las empresas; que incidirán 
+                        ​positivamente en el desarrollo de las regiones como contribución a una Colombia educada, equitativa y en paz.​
+               </p>
+            </div>
+        </div>   
+      </div>   
 
 
-
-
-
-
-
-
-
-
-
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+<br><br>
 <br>
 
 
